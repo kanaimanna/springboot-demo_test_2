b3f63ffe888ee834ad97dabf89e0f96e90522047
kanaimanna <kanaimanna@gmail.com>
2024-02-18 21:08:38 +0530

    My1st project

---
+++ src/main/java/com/star/agile/assignment/springbootdemo_test_2/SpringbootDemoTest2Application.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment.springbootdemo_test_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/star/agile/assignment/springbootdemo_test_2/SpringbootDemoTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment.springbootdemo_test_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
